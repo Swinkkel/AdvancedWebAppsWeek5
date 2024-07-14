f9c52f13d94abbc7a8b4355bfcd8177a2532606d
Tomi Helin <tomi.helin@student.lut.fi>
2024-07-14 16:15:36 +0300

    test

--- models/recipe.js
+++ models/recipe.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const recipeSchema = new mongoose.Schema({
+let recipeSchema = new mongoose.Schema({
   name: String,
   instructions: [String],
   ingredients: [String],
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
 
-module.exports = Recipe;
+
+module.exports = mongoose.model('Recipe', recipeSchema);
